e60a0f52b908cd279263d2fa6fde43da6ecdd1e8
sidneiandre <sidneiandre1988@gmail.com>
2024-11-23 18:46:38 -0300

    codigo-fonte
    passando o código para o repositório

---
+++ index.js
@@ -0,0 +1,22 @@
+let nomeHeroi = "Pegazus"
+let xpHeroi = 10.000
+
+if (xpHeroi >= 1.000) {
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Ferro")
+}else if (1.001 >= 2.000) {
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Bronze")
+}else if (2.001 >= 5.000){
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Prata")
+}else if (5.001 >= 7.000){
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Ouro")
+}else if (7.001 >= 8.000){
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Platina")
+}else if (8.001 >= 9.000) {
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Ascendente")
+}else if (9.001 >= 10.000){
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Imortal")
+}else{
+    console.log("O Héroi de nome " + nomeHeroi + "está no nível  Radiante")
+}
+
+
